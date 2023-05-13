cf210b162aae3e93f7399d8ad7918b1733015a96
paul5130 <paul51305040@gmail.com>
2023-05-13 11:27:13 +0800

    [feat] 移除storyboard

---
+++ xaregroup.interview/xaregroup.interview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xaregroup.interview
+//
+//  Created by Paul Wen on 2023/5/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
