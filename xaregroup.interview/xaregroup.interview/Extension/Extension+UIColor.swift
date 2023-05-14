86bb8e78fbb5fbe28d344d9f58e89311c52bd407
paul5130 <paul51305040@gmail.com>
2023-05-14 10:28:49 +0800

    [feat] 實作Extension

---
+++ xaregroup.interview/xaregroup.interview/Extension/Extension+UIColor.swift
@@ -0,0 +1,21 @@
+//
+//  Extension+UIColor.swift
+//  xaregroup.interview
+//
+//  Created by Paul Wen on 2023/5/14.
+//
+
+import UIKit
+
+extension UIColor{
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor{
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    static func tabbarUnselectColor() -> UIColor{ .rgb(red: 205, green: 205, blue: 205)}
+    static func blackBackgroundColor() -> UIColor{ .rgb(red: 47, green: 40, blue: 40)}
+    static func grayBackgroundColor() -> UIColor{ .rgb(red: 239, green: 239, blue: 239)}
+    static func grayTextColor() -> UIColor{ .rgb(red: 148, green: 147, blue: 147)}
+    static func linearGradientColor1() -> UIColor{ .rgb(red: 236, green: 205, blue: 95)}
+    static func linearGradientColor2() -> UIColor{ .rgb(red: 197, green: 255, blue: 123)}
+}
+
---
+++ xaregroup.interview/xaregroup.interview/Extension/Extension+UIImage.swift
@@ -0,0 +1,19 @@
+//
+//  Extension+UIImage.swift
+//  xaregroup.interview
+//
+//  Created by Paul Wen on 2023/5/14.
+//
+
+import UIKit
+
+extension UIImage{
+    static func man() -> UIImage{ #imageLiteral(resourceName: "man") }
+    static func nikeLogo() -> UIImage{ #imageLiteral(resourceName: "nike_logo") }
+    static func nikeShoe() -> UIImage{ #imageLiteral(resourceName: "nike_shoe") }
+    static func tabCart() -> UIImage{ #imageLiteral(resourceName: "tab_cart") }
+    static func tabHome() -> UIImage{ #imageLiteral(resourceName: "tab_home") }
+    static func tabUser() -> UIImage{ #imageLiteral(resourceName: "tab_user") }
+    static func tabWallet() -> UIImage{ #imageLiteral(resourceName: "tab_wallet") }
+    static func woman() -> UIImage{ #imageLiteral(resourceName: "woman") }
+}
