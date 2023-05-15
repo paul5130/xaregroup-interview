66df161b0ea072feb48ee9629d036106ca145ef9
paul5130 <paul51305040@gmail.com>
2023-05-15 09:14:09 +0800

    [feat] 實作extension

--- xaregroup.interview/xaregroup.interview/Extension/Extension+UIColor.swift
+++ xaregroup.interview/xaregroup.interview/Extension/Extension+UIColor.swift
@@ -8,8 +8,8 @@
 import UIKit
 
 extension UIColor{
-    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor{
-        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat,alpha: CGFloat = 1) -> UIColor{
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: alpha)
     }
     static func tabbarUnselectColor() -> UIColor{ .rgb(red: 205, green: 205, blue: 205)}
     static func blackBackgroundColor() -> UIColor{ .rgb(red: 47, green: 40, blue: 40)}
--- xaregroup.interview/xaregroup.interview/Extension/Extension+UIImage.swift
+++ xaregroup.interview/xaregroup.interview/Extension/Extension+UIImage.swift
@@ -16,4 +16,26 @@ extension UIImage{
     static func tabUser() -> UIImage{ #imageLiteral(resourceName: "tab_user") }
     static func tabWallet() -> UIImage{ #imageLiteral(resourceName: "tab_wallet") }
     static func woman() -> UIImage{ #imageLiteral(resourceName: "woman") }
+    static func heart() -> UIImage{ #imageLiteral(resourceName: "heart") }
+}
+extension UIImage{
+    func rotate(radians: CGFloat) -> UIImage {
+        let rotatedSize = CGRect(origin: .zero, size: size)
+            .applying(CGAffineTransform(rotationAngle: CGFloat(radians)))
+            .integral.size
+        UIGraphicsBeginImageContext(rotatedSize)
+        if let context = UIGraphicsGetCurrentContext() {
+            let origin = CGPoint(x: rotatedSize.width / 2.0,
+                                 y: rotatedSize.height / 2.0)
+            context.translateBy(x: origin.x, y: origin.y)
+            context.rotate(by: radians)
+            draw(in: CGRect(x: -origin.y, y: -origin.x,
+                            width: size.width, height: size.height))
+            let rotatedImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            
+            return rotatedImage ?? self
+        }
+        return self
+    }
 }
